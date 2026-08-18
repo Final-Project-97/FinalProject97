c4d7fc4f540c10589e752f37ae5f14beef4df262
NabhanRI <nabhanrifai@gmail.com>
2026-08-18 20:10:24 +0700

    refactor(home): update homepage and product 360 section

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -2,16 +2,16 @@ import FloatAI from "../components/home/FloatAI";
 import Hero from "../components/home/Hero";
 import Product360 from "../components/home/Product360";
 import Stats from "../components/home/Stasts";
+import PreFooter from "../components/home/PreFooter";
 
 export default function Home() {
   return (
-    <>
-      <div className="bg-[#141620]">
-        <Hero />
-        <Stats />
-        <Product360 />
-        <FloatAI />
-      </div>
-    </>
+    <div className="bg-[#0C0E16] min-h-screen">
+      <Hero />
+      <Stats />
+      <Product360 />
+      <PreFooter />
+      <FloatAI />
+    </div>
   );
 }
